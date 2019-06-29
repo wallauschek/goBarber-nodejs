9959509434e302faec477c9e566fe8d157deedee
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2019-06-29 14:28:10 -0300

    Notificações lidas. Mens de conf de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '537c465cdf1fa7',
+    pass: '2d0067a6b8e60b',
+  },
+  defaul: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -25,10 +25,12 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schendule', SchenduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
