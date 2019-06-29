5b6b4d92bd90a5e4da3d6d87394b0755b62fd631
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2019-06-28 23:10:36 -0300

    Finalizando Agendamento e Agenda do Provider

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import SchenduleController from './app/controllers/SchenduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -21,7 +22,10 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
-routes.get('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schendule', SchenduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
