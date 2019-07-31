c4ff917c89a327a42a15281358da735ec0e47c20
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2019-07-30 23:04:48 -0300

    correções e liberando cors

--- src/app/controllers/SchenduleController.js
+++ src/app/controllers/SchenduleController.js
@@ -28,6 +28,13 @@ class SchenduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
       order: ['date'],
     });
 
